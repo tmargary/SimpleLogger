b3de999a2721f37ccc902491dfd990c52926be09
Tigran Margaryan <tigranmargarian@outlook.com>
2023-02-14 01:59:02 +0400

    attempts to fix the win build issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <limits.h>
-#include <libgen.h>
 #include <nlohmann/json.hpp>
 #include <filesystem>
+
+#ifdef _WIN32
+#include <windows.h>
+#define getcwd _getcwd
+#else
+#include <unistd.h>
+#include <libgen.h>
+#endif
+
 namespace fs = std::filesystem;
 
 #include <Logger/Logger.h>
@@ -12,9 +20,21 @@ namespace fs = std::filesystem;
 std::string get_cwd()
 {
   char path[PATH_MAX];
+#ifdef _WIN32
+  char drive[MAX_PATH];
+  char dir[MAX_PATH];
+  char filename[MAX_PATH];
+  char extension[MAX_PATH];
+
+  GetModuleFileName(NULL, path, MAX_PATH);
+  _splitpath_s(path, drive, MAX_PATH, dir, MAX_PATH, filename, MAX_PATH, extension, MAX_PATH);
+
+  return std::string(drive) + std::string(dir);
+#else
   strncpy(path, __FILE__, sizeof(path));
   char *dir = dirname(path);
   return dir;
+#endif
 }
 
 LogLevel get_level(const std::string &config_path)

69c87126c420537f2a41c6ab9ca09546cf099c84
Tigran Margaryan <tigranmargarian@outlook.com>
2023-02-14 17:58:53 +0400

    localtime_s and localtime_r support for win and linux

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -6,24 +6,31 @@
 
 std::string DateTimeNow(const char *format = "%Y-%m-%d %X")
 {
-  auto now = std::chrono::system_clock::now();
-  auto in_time_t = std::chrono::system_clock::to_time_t(now);
-  std::tm tm;
-  localtime_r(&in_time_t, &tm);
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    std::tm tm;
 
-  std::stringstream ss;
-  char buffer[80];
-  strftime(buffer, 80, format, &tm);
-  ss << buffer;
-  return ss.str();
+#ifdef _WIN32
+    localtime_s(&tm, &in_time_t);
+#else
+    localtime_r(&in_time_t, &tm);
+#endif
+
+    std::stringstream ss;
+    char buffer[80];
+    strftime(buffer, 80, format, &tm);
+    ss << buffer;
+    return ss.str();
 }
 
 std::string get_cwd()
 {
     std::filesystem::path current_path = std::filesystem::current_path();
-    while (!std::filesystem::exists(current_path / "CMakeLists.txt")) {
+    while (!std::filesystem::exists(current_path / "CMakeLists.txt"))
+    {
         current_path = current_path.parent_path();
-        if (current_path.empty()) {
+        if (current_path.empty())
+        {
             throw std::runtime_error("Error: could not find project root directory");
         }
     }
